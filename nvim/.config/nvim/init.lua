329448e667f72f55a2567031ce48c4a34b75e588
Kyle Stublefield <iamtuxmeister@gmail.com>
2025-10-25 17:13:56 -0700

    fixed nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -541,31 +541,6 @@ local mason_lspconfig = require 'mason-lspconfig'
 mason_lspconfig.setup {
   ensure_installed = vim.tbl_keys(servers),
 }
-
-<<<<<<< HEAD
--- mason_lspconfig.setup_handlers {
---   function(server_name)
---     require('lspconfig')[server_name].setup {
---       capabilities = capabilities,
---       on_attach = on_attach,
---       settings = servers[server_name],
---       filetypes = (servers[server_name] or {}).filetypes,
---     }
---   end
--- }
-=======
---!! mason_lspconfig.setup_handlers {
---!!   function(server_name)
---!!     require('lspconfig')[server_name].setup {
---!!       capabilities = capabilities,
---!!       on_attach = on_attach,
---!!       settings = servers[server_name],
---!!       filetypes = (servers[server_name] or {}).filetypes,
---!!     }
---!!   end
---!! }
->>>>>>> 351f28dc445682a0d048914f5cb0730715f722c1
-
 -- Setup LSP for Gleam
 -- require('lspconfig').gleam.setup({})
 
